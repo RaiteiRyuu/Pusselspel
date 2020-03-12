87566861e15bce2190218d52b09279d784bace2a
Ryuu <evil__dark__angel@hotmail.com>
2020-03-12 08:18:09 +0100

    testing upload files

---
+++ Puzzle jigsaw/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Puzzle_jigsaw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onclick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open_File = new OpenFileDialog();
+            open_File.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            if (open_File.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(open_File.FileName));
+            }
+        }
+    }
+}
